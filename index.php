b1f68b3b6b6c2b6f65688e5da32a7fd2902daf59
Gungcakra <agungcakra888@gmail.com>
2024-12-24 10:26:51 +0800

    update index.php title and adjust asset paths

--- index.php
+++ index.php
@@ -49,10 +49,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>SB Admin 2 - Login</title>
+    <title>SION</title>
     <link href="<?= BASE_URL_HTML ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link href="<?= BASE_URL_HTML ?>/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?= BASE_URL_HTML ?>/assets/css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 
 <body class="bg-gradient-primary">
@@ -94,7 +94,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <script src="<?= BASE_URL_HTML ?>/vendor/jquery/jquery.min.js"></script>
     <script src="<?= BASE_URL_HTML ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="<?= BASE_URL_HTML ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="<?= BASE_URL_HTML ?>/js/sb-admin-2.min.js"></script>
+    <script src="<?= BASE_URL_HTML ?>/assets/js/sb-admin-2.min.js"></script>
 
 </body>
 
